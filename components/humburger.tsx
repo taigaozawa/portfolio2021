8b06070b827ee968f6aef0907a4427472f416e82
taigaozawa <taigaozawa@gmail.com>
2021-08-07 18:01:15 +0900

    top page

---
+++ components/humburger.tsx
@@ -0,0 +1,11 @@
+const Humburger = ({ className }) => (
+  <div>
+    <svg viewBox="0 0 30 30" width="20" height="20" className={className}>
+      <rect y="10" width="30" height="2"></rect>
+      <rect y="19" width="30" height="2"></rect>
+      <rect y="28" width="30" height="2"></rect>
+    </svg>
+  </div>
+);
+
+export default Humburger;
